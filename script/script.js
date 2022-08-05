1ef64539d1fd1339a97965474547741eec9b4943
Tommaso Piccioni <tommaso.piccioni.cr@gmail.com>
2022-08-05 12:21:10 +0200

    Improves organization by separating the different functions from te main one

--- script/script.js
+++ script/script.js
@@ -1,6 +1,8 @@
 // PROBLEMS / TODO:
 // button to change the sign
 // link the display with the current focused element/result
+// A/C button
+// delete button
 
 
 // --------------------------------------------------------
@@ -29,85 +31,119 @@ let lastInput = "";
 function calculator() {
     // DIGITS
     digits.forEach((digit) => {
-        digit.addEventListener("click", (e) => {
-            if (firstVar) {  // if we're working with the first variable (true), add the digit input to var1
-                result = 0;  // after one operation finished with "=", if the user wants to starts a new one and starts typing, the previous operation's result is re-initialized 
-                var1 += e.target.getAttribute("data-value");
-            } else {  // if we're working with the second variable (false), add the digit input to var2
-                var2 += e.target.getAttribute("data-value");
-            };
-            // variable used when clicking an operator
-            lastInput = "digit";
-
-            // Test -----------------
-            console.log("digit click -------------------------")
-            console.log("last input type: " + lastInput)
-            console.log("var1: " + var1);
-            console.log("var2: " + var2);
-            console.log("op: " + op);
-            console.log("firstVar: " + firstVar);
-            console.log("result: " + result);
-        });
+        digit.addEventListener("click", digitPressed);
     });
     // OPERATORS
     operators.forEach((operator) => {
-        operator.addEventListener('click', (e) => {
-            // if the user, after one operation finished with "=" so the result variable results TRUE, wants to continue using
-            // the result as the starting point for a new operation, pressing a operation sign will start a new cycle assigning
-            // the previous operation's result to var1
-            if (result) {  
-                var1 = result;
-            };
-            // if the user wants to concatenate multiple operations (so a value has already been assigned to var1 and var2),
-            // the previous operation's result is assigned to var1 and var2 is initialized for the next digit
-            if (var1 && var2) {  
-                var1 = operate(parseInt(var1), parseInt(var2), op);
-                var2 = "";
-                // when the condition is true, this changes only to equilibrate the second change below
-                firstVar = !firstVar;
-            };
-            // the operation the user wants to perform is assigned to the "op" variable
-            op = e.target.getAttribute("data-value");  
-            // the user can change the operator (es. + -> /) without changing the variable he's working on (var2)
-            if (lastInput != "operator") {
-                firstVar = !firstVar;
-            };
-            lastInput = "operator";
-            
-            // Test -----------------
-            console.log("operator click -------------------------")
-            console.log("last input type: " +lastInput)
-            console.log("var1: " + var1);
-            console.log("var2: " + var2);
-            console.log("op: " + op);
-            console.log("firstVar: " + firstVar);
-            console.log("result: " + result);
-        });
+        operator.addEventListener('click', operatorPressed);
     });
     // EQUAL SIGN
-    equal.addEventListener('click', () => {
+    equal.addEventListener('click', equalSignPressed);
+    // CLEAR
+    clear.addEventListener("click", restart)
+};
+
+// --------------------------------------------------------
+// OTHER FUNCTIONS
+
+// the user press a operator sign (+,-,*,/)
+function operatorPressed(e) {
+    // if the user, after one operation finished with "=" so the result variable results TRUE, wants to continue using
+    // the result as the starting point for a new operation, pressing a operation sign will start a new cycle assigning
+    // the previous operation's result to var1
+    if (result) {
+        var1 = result;
+    };
+    // if the user wants to concatenate multiple operations (so a value has already been assigned to var1 and var2),
+    // the previous operation's result is assigned to var1 and var2 is initialized for the next digit
+    if (var1 && var2) {
+        var1 = operate(parseInt(var1), parseInt(var2), op);
+        var2 = "";
+        // when the condition is true, this changes only to equilibrate the second change below
+        firstVar = !firstVar;
+    };
+    // the operation the user wants to perform is assigned to the "op" variable
+    op = e.target.getAttribute("data-value");
+    // the user can change the operator (es. + -> /) without changing the variable he's working on (var2)
+    if (lastInput != "operator") {
+        firstVar = !firstVar;
+    };
+    lastInput = "operator";
+
+    // Test -----------------
+    console.log("operator click -------------------------")
+    console.log("last input type: " + lastInput)
+    console.log("var1: " + var1);
+    console.log("var2: " + var2);
+    console.log("op: " + op);
+    console.log("firstVar: " + firstVar);
+    console.log("result: " + result);
+}
+
+// the user press a digit (0-9)
+function digitPressed(e) {
+    if (firstVar) {  // if we're working with the first variable (true), add the digit input to var1
+        result = 0;  // after one operation finished with "=", if the user wants to starts a new one and starts typing, the previous operation's result is re-initialized 
+        var1 += e.target.getAttribute("data-value");
+    } else {  // if we're working with the second variable (false), add the digit input to var2
+        var2 += e.target.getAttribute("data-value");
+    };
+    // variable used when clicking an operator
+    lastInput = "digit";
+
+    // Test -----------------
+    console.log("digit click -------------------------")
+    console.log("last input type: " + lastInput)
+    console.log("var1: " + var1);
+    console.log("var2: " + var2);
+    console.log("op: " + op);
+    console.log("firstVar: " + firstVar);
+    console.log("result: " + result);
+
+}
+
+// the user press the equal sign
+function equalSignPressed(e) {
+    if (var1 && var2 && op) {
         result = operate(parseInt(var1), parseInt(var2), op);
         // all the variables are re-initialized for the next operation
         op = "";
         var1 = "";
         var2 = "";
         firstVar = !firstVar;
-
-        // Test -----------------
-        console.log("= click -------------------------")
-        console.log("last input type: " + lastInput)
-        console.log("var1: " + var1);
-        console.log("var2: " + var2);
-        console.log("op: " + op);
-        console.log("firstVar: " + firstVar);
-        console.log("result: " + result);
-    });
-};
+    }
 
 
+    // Test -----------------
+    console.log("= click -------------------------")
+    console.log("last input type: " + lastInput)
+    console.log("var1: " + var1);
+    console.log("var2: " + var2);
+    console.log("op: " + op);
+    console.log("firstVar: " + firstVar);
+    console.log("result: " + result);
+}
 
-// --------------------------------------------------------
-// OTHER FUNCTIONS
+// the user press A/C 
+function restart() {
+    console.log("restart")
+    let firstVar = true;
+    let var1 = "";
+    let var2 = "";
+    let op = "";
+    let result = "";
+    let lastInput = "";
+    calculator()
+
+    // Test -----------------
+    console.log("clear click -------------------------")
+    console.log("last input type: " + lastInput)
+    console.log("var1: " + var1);
+    console.log("var2: " + var2);
+    console.log("op: " + op);
+    console.log("firstVar: " + firstVar);
+    console.log("result: " + result);
+}
 
 // takes two input values and one operation function (below) and return the result
 function operate(input1, input2, operationToDo) {
@@ -119,6 +155,7 @@ function operate(input1, input2, operationToDo) {
 function displayResult(toDisplay) {
     display.textContent = toDisplay;
 }
+
 
 // --------------------------------------------------------
 // OPERATION FUNCTIONS
